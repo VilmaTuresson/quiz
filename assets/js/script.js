6b46ab18f0a45dcefd861185609bec05cf5cc9ba
VilmaTuresson <vilma.turesson@gmail.com>
2021-11-25 12:27:48 +0000

    add 'next' btn and js to get question with options

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,41 @@
 let startQuiz = document.getElementById('start');
-startQuiz.addEventListener('click', startFunc)
+let quizHolder = document.getElementById('quiz_holder');
 
 function startFunc() {
-    console.log('hey');
+    startQuiz.style.display = 'none'
+    quizHolder.classList.remove('hidden')
 }
+
+startQuiz.addEventListener('click', startFunc)
+
+
+
+
+
+
+function nextQ() {
+    let q = {
+        question: 'Which English football team is called the Magpies?',
+        option: ['Chelsea', 'Everton', 'Newcastle', 'Stroke'],
+        correctAnswer: 2
+    }
+    
+    let questionGet = document.getElementById('question');
+    questionGet.textContent = q.question;
+
+    let optionsGet = document.querySelectorAll('.option');
+    optionsGet.forEach(function(element, index) {
+        element.textContent = q.option[index];
+
+        element.addEventListener('click', function() {
+            if(q.correctAnswer == index) {
+                console.log('right')       
+            } else {
+                console.log('wrong')
+            }
+        })
+    });
+} 
+
+nextQ(question);
+
